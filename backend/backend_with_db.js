f30d17d69caa225e849263cf03039bada539eeec
Charlie Ward <charles.a.ward00@gmail.com>
2021-10-22 11:46:22 -0700

    Created basic database schemas

---
+++ backend/backend_with_db.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+const userModel = require("./models/user");
+
+const app = express();
+const port = 5000;
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+    'mongodb://localhost:27017/users',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  ).catch(error => console.log(error));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  Image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  Listings: [{
+    type: Schema.Types.ObjectId,
+    ref: "Listing"
+  }]
+}, {collection : 'users_list'});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
