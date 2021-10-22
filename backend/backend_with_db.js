a26d39a74bdd86190e12553d374910687b8c5800
Charlie Ward <charles.a.ward00@gmail.com>
2021-10-22 11:49:43 -0700

    basic backend

--- backend/backend_with_db.js
+++ backend/backend_with_db.js
@@ -22,3 +22,7 @@ mongoose.connect(
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
