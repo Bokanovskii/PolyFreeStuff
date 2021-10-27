a6435e8d32ef298249e95173ef4733c1bdcafd17
Charlie Ward <charles.a.ward00@gmail.com>
2021-10-27 11:46:30 -0700

    user creation function and style fixes

--- backend/backend_with_db.js
+++ backend/backend_with_db.js
@@ -1,78 +1,91 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
 const userModel = require("./models/user");
-const listingModel = require('./models/listing');
+const listingModel = require("./models/listing");
 
 const app = express();
 const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/free_stuff',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).catch(error => console.log(error));
+mongoose
+  .connect("mongodb://localhost:27017/free_stuff", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+async function createNewUser(email) {
+  const basic_user = {
+    name: "New User",
+    email: email,
+    image: "",
+    listings: [],
+  };
+  // if correctly placed in the database, return the created usr
+  //    will have a '_id' variable appended as part of the addUser call
+  if (await addUser(basic_user)) return basic_user;
+  else return null;
+}
+
 //dummy function
-app.get('/', async (req, res) => {
-    //res.send('Hello World!');
-    const users_from_db = await userModel.find();
-    res.send({users_list: users_from_db});
-    
+app.get("/", async (req, res) => {
+  //res.send('Hello World!');
+  const users_from_db = await userModel.find();
+  res.send({ users_list: users_from_db });
 });
+
 //Dummy function
-app.post('/test_add_user', async (req, res) => {
-    const user = req.body;
-    if (await add_user(user))
-        res.status(201).send(user)
-    else
-        res.status(500).send(user)
+app.post("/test_add_user", async (req, res) => {
+  const user = req.body;
+  if (await add_user(user)) res.status(201).send(user);
+  else res.status(500).send(user);
 });
+
 // Dummy function
-app.post('/test_add_listing', async (req, res) => {
-    const listing = req.body;
-    // General format:
-    // let listing = {'name': 'listing #', 'description': 'haha', 'Seller': user['_id'], 'is_available': true, 'creation_date': Date.now()}
-    if (await add_listing(listing))
-        res.status(201).send(listing)
-    else
-        res.status(500).send(listing)
+app.post("/test_add_listing", async (req, res) => {
+  const listing = req.body;
+  // General format:
+  // let listing = {'name': 'listing #', 'description': 'haha', 'seller': user['_id'], 'is_available': true, 'creation_date': Date.now()}
+  if (await add_listing(listing)) res.status(201).send(listing);
+  else res.status(500).send(listing);
 });
+
 // Basic implementation for this (may need more work)
-async function add_user(user){
-    try{
-        const userToAdd = new userModel(user);
-        let added_user = await userToAdd.save();
-        user['_id'] = added_user._id;
-        return true;
-    } catch(error) {
-        console.log(error);
-        return false;
-    }   
+async function add_user(user) {
+  try {
+    const userToAdd = new userModel(user);
+    let added_user = await userToAdd.save();
+    user["_id"] = added_user._id;
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
+
 // Basic implementation for this (may need more work)
-async function add_listing(listing){
-    try{
-        // add listing to the database
-        const ListingToAdd = new listingModel(listing);
-        let added_listing = await ListingToAdd.save();
-        listing['_id'] = added_listing._id;
-        // Update the user with the listing
-        const user = await userMode.findById(listing['Seller'])
-        user['Listings'] += added_listing._id
-        await userModel.findByIdAndUpdate(user['_id'], user);
-        return true;
-    }catch(error) {
-        console.log(error);
-        return false;
-    }   
+async function add_listing(listing) {
+  try {
+    // add listing to the database
+    const ListingToAdd = new listingModel(listing);
+    let added_listing = await ListingToAdd.save();
+    listing["_id"] = added_listing._id;
+    // Update the user with the listing
+    const user = await userMode.findById(listing["seller"]);
+    user["listings"] += added_listing._id;
+    await userModel.findByIdAndUpdate(user["_id"], user);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
- 
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
       required: true,
       trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    listings: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Listing",
+      },
+    ],
   },
-  Image: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  Listings: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Listing"
-  }]
-}, {collection : 'users_list'});
+  { collection: "users_list" }
+);
 
 const User = mongoose.model("User", UserSchema);
 
