1af8258092c2a9c556e6bb02b2c235a4fdc1f123
Nathan McCutchen <n8wm@icloud.com>
2021-10-29 11:05:25 -0700

    Adding homepage navbar stuff

---
+++ frontend/src/home-page/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../navigation-buttons/navigation-buttons';
+
+function Homepage() {
+    return (
+        <NavBar />
+    );
+}
+
+export default Homepage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
