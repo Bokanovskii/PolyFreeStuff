d0eae02c07aa891be3dfb1fc3322f3e7b6498a6b
green <ewitulsk@calpoly.edu>
2021-10-31 23:15:26 -0700

    Redirects users to homepage after successful login

--- frontend/src/login-page/Login.js
+++ frontend/src/login-page/Login.js
@@ -1,9 +1,11 @@
 import {useState} from "react";
 import axios from "axios";
+import {useHistory} from "react-router-dom";
 
 function Login(props){
 
     const [validEmail, setValidEmail] = useState(null);
+    const history = useHistory();
 
     async function login (email, setEmail, setLoggedIn) {
         //Make get req here when backend ready
@@ -14,6 +16,7 @@ function Login(props){
                     console.log("Good login")
                     setEmail(email);
                     setLoggedIn(true);
+                    history.push('/my-listings')
                 }
             }
         ).catch((error) => {
