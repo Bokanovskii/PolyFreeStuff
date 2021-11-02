2688f633d97a47cb7d42b10dc032ffd9d746c4c4
Nathan McCutchen <n8wm@icloud.com>
2021-11-01 23:47:57 -0700

    Implemented localStorage with login info

--- frontend/src/account-info/account-info.js
+++ frontend/src/account-info/account-info.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-function AccountInfo() {
-    return (
-        <div id="account-info-page">ACCOUNT INFO</div>
-    );
+function AccountInfo(props) {
+  return (
+    <div id="account-info-page">
+      <button
+        id="logout"
+        onClick={() => {
+          props.logout();
+        }}
+      >
+        Log Out
+      </button>
+    </div>
+  );
 }
 
 export default AccountInfo;
--- frontend/src/login-page/Login.js
+++ frontend/src/login-page/Login.js
@@ -1,70 +1,80 @@
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-function Login(props){
+function Login(props) {
+  const [validEmail, setValidEmail] = useState(null);
+  let history = useHistory();
 
-    const [validEmail, setValidEmail] = useState(null);
-    let history = useHistory();
-
-    async function login (email, setEmail, setLoggedIn) {
-        //Make get req here when backend ready
-        await axios.get('http://localhost:5000/login/'.concat(email.toString()))
-            .then((resonse) => {
-                let status = resonse.status;
-                if(status === 201){
-                    console.log("Good login")
-                    setEmail(email);
-                    setLoggedIn(true);
-                    history.push('/');
-                }
-            }
-        ).catch((error) => {
-            window.alert(error.toString());
-            });
-    }
-
-    function checkLoggedIn(){
-        console.log(props.email)
-        if(props.loggedIn){
-            history.push('/');
-            return(<span>Successfully Logged In</span>);
+  async function login(email, setEmail, setLoggedIn) {
+    //Make get req here when backend ready
+    await axios
+      .get("http://localhost:5000/login/".concat(email.toString()))
+      .then((response) => {
+        let status = response.status;
+        if (status === 201) {
+          console.log("Good login");
+          setEmail(email);
+          localStorage.setItem("email", email);
+          setLoggedIn(true);
+          history.push("/");
         }
-        return;
-    }
+      })
+      .catch((error) => {
+        window.alert(error.toString());
+      });
+  }
 
-    function checkValidEmail(){
-        if(validEmail != null && !validEmail){
-            return <span>Invalid email, make sure it ends with "@calpoly.edu" </span>
-        }
+  function checkLoggedIn() {
+    console.log(props.email);
+    const loggedInEmail = localStorage.getItem("email");
+    if (props.loggedIn || loggedInEmail) {
+      history.push("/");
+      return <span>Successfully Logged In</span>;
     }
+    return;
+  }
 
-    function handleSubmit(e){
-        e.preventDefault();
-        let email = e.target.email.value;
-        let endsWithCPEmail = email.endsWith("@calpoly.edu");
-        let emailLongerThan0 = email.substr(0, email.indexOf('@')).length > 0;
-        if(endsWithCPEmail && emailLongerThan0){
-            setValidEmail(true);
-            login(email, props.setEmail, props.setLoggedIn);
-        } else {
-            setValidEmail(false);
-        }
+  function checkValidEmail() {
+    if (validEmail != null && !validEmail) {
+      return <span>Invalid email, make sure it ends with "@calpoly.edu" </span>;
     }
+  }
 
-    return(
-        <div id="login-page">
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <label>Email: </label>
-                <input type={"text"} id={"login-email"} name={"email"} placeholder="username@calpoly.edu" />
-                <button type={"submit"} id={"login-btn"}>Sign In</button>
-            </form>
-            <p>&emsp;
-                {checkLoggedIn()}
-                {checkValidEmail()}
-            </p>
-        </div>
-    );
+  function handleSubmit(e) {
+    e.preventDefault();
+    let email = e.target.email.value;
+    let endsWithCPEmail = email.endsWith("@calpoly.edu");
+    let emailLongerThan0 = email.substr(0, email.indexOf("@")).length > 0;
+    if (endsWithCPEmail && emailLongerThan0) {
+      setValidEmail(true);
+      login(email, props.setEmail, props.setLoggedIn);
+    } else {
+      setValidEmail(false);
+    }
+  }
+
+  return (
+    <div id="login-page">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>Email: </label>
+        <input
+          type={"text"}
+          id={"login-email"}
+          name={"email"}
+          placeholder="username@calpoly.edu"
+        />
+        <button type={"submit"} id={"login-btn"}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        &emsp;
+        {checkLoggedIn()}
+        {checkValidEmail()}
+      </p>
+    </div>
+  );
 }
 
 export default Login;
