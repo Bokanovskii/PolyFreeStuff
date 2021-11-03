59649fde20d7fc56b3ce306be4c6c0a7d3021b1b
Charlie Ward <charles.a.ward00@gmail.com>
2021-11-03 11:15:43 -0700

    added package.json

--- backend/models/listing.js
+++ backend/models/listing.js
@@ -1,45 +1,48 @@
 const mongoose = require("mongoose");
 
-const ListingSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  pickup_location: {
+const ListingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
       type: String,
       required: false,
       trim: true,
-  },
-  image: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  seller: {
+    },
+    pickup_location: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    seller: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: 'User',
-  },
-  buyer: {
+      ref: "User",
+    },
+    buyer: {
       type: mongoose.Schema.Types.ObjectId,
       required: false,
-      ref: 'User',
-  },
-  is_available: {
+      ref: "User",
+    },
+    is_available: {
       type: Boolean,
       required: true,
-  },
-  creation_date: {
+    },
+    creation_date: {
       type: Date,
       required: true,
-  }
-}, {collection : 'Listings'});
+    },
+  },
+  { collection: "Listings" }
+);
 
 const Listing = mongoose.model("Listing", ListingSchema);
 
