1f1ae78cf3992bca1f1cbb65f42b25fd883ed28d
green <ewitulsk@calpoly.edu>
2021-11-08 01:19:34 -0800

    My listings page now retrieves all of a user's listings

--- frontend/src/my-listings-page/my-listings.js
+++ frontend/src/my-listings-page/my-listings.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreateListing from "./create-listing";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -7,17 +7,74 @@ import settings from "../settings";
 function MyListings(props) {
   const [listings, setListings] = useState([]);
 
-  /*
-    async function getUserListingIds(userId){
-        await axios
-            .get(settings.URLBase.concat("/users/").concat(userId))
-            .then((response))
-    }*/
+  let listingsIds;
+  let listingsObjs = [];
+
+  async function getUserListingIds(userId) {
+    await axios
+      .get(settings.URLBase.concat("/user/").concat(userId))
+      .then((response) => {
+        let status = response.status;
+        if (status === 201) {
+          listingsIds = response.data.listings;
+          //console.log(listingsIds);
+          //console.log("Listings Idss: "+ listingsIds);
+        }
+      })
+      .catch((error) => {
+        window.alert(error.toString());
+      });
+  }
+
+  async function appendListing(listingId) {
+    await axios
+      .get(settings.URLBase.concat("/listing/").concat(listingId))
+      .then((response) => {
+        let status = response.status;
+        if (status === 201) {
+          //console.log(response.data.listing_from_db);
+          listingsObjs.push(response.data.listing_from_db);
+        }
+      })
+      .catch((error) => {
+        window.alert(error.toString());
+      });
+  }
+
+  async function populateListingObjs() {
+    await getUserListingIds(props.userData._id);
+    for (let i = 0; i < listingsIds.length; i++) {
+      await appendListing(listingsIds[i]);
+      //console.log("Listing Id: "+listingsIds[i]);
+    }
+    setListings(listingsObjs);
+    //console.log("Listing Objects: "+listingsObjs);
+  }
+
+  useEffect(() => {
+    populateListingObjs();
+  }, []);
 
   return (
     <div id="my-listings-page" className="usr-page">
       <h1>My Listings</h1>
-      <Link to={"/create-listing"}><button>Create New Listing</button></Link>
+
+      <div>
+        {listings.map((listing) => (
+          <div>
+            <label>Name: </label>
+            <div>{listing.name}</div>
+            <label>Description: </label>
+            <div>{listing.description}</div>
+            <label>Is Available: </label>
+            <div>{listing.is_available.toString()}</div>
+            <br />
+          </div>
+        ))}
+      </div>
+      <Link to={"/create-listing"}>
+        <button>Create New Listing</button>
+      </Link>
     </div>
   );
 }
