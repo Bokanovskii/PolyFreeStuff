8a3f7170ab01ba7eaf563d8fd983b4db1bdac0c6
Nathan McCutchen <n8wm@icloud.com>
2021-11-17 11:30:51 -0800

    broken commit

--- frontend/src/home-page/homepage.js
+++ frontend/src/home-page/homepage.js
@@ -5,24 +5,41 @@ import ListingGrid from "../listings/listing-grid";
 
 function Homepage() {
   const [listings, setListings] = useState([]);
+  const [page, setPage] = useState(0);
+  const [numPages, setNumPages] = useState(null);
+  const pageSize = 10;
 
-  async function getAllListings() {
-    await axios
-      .get(settings.URLBase.concat(`/listings?getUser=${true}`))
-      .then((response) => {
-        let status = response.status;
-        if (status === 201) {
-          setListings(response.data.listing_list);
-        }
-      })
-      .catch((error) => {
-        window.alert(error.toString());
-      });
+  function prevPage() {
+    if (page > 0) setPage(page - 1);
+  }
+
+  function nextPage() {
+    if (page < numPages) setPage(page + 1);
   }
 
   useEffect(() => {
+    async function getAllListings() {
+      setNumPages(5);
+      await axios
+        .get(
+          settings.URLBase.concat(
+            `/listings?getUser=${true}&start=${page * pageSize}&end=${
+              (page + 1) * pageSize
+            }`
+          )
+        )
+        .then((response) => {
+          let status = response.status;
+          if (status === 201) {
+            setListings(response.data.listing_list);
+          }
+        })
+        .catch((error) => {
+          window.alert(error.toString());
+        });
+    }
     getAllListings();
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -33,6 +50,14 @@ function Homepage() {
           itemPath={"/homepage/listing/:itemID"}
           max="20"
         />
+        <div id="pagination">
+          <button onClick={prevPage()}>
+            <span className="material-icons">arrow_back_ios_new</span>Prev
+          </button>
+          <button onClick={nextPage()}>
+            <span className="material-icons">arrow_forward_ios_new</span>Next
+          </button>
+        </div>
       </div>
     </div>
   );
