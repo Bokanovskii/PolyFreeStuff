c1ccb19e06fe75abd1080c81513dcb8a046a7860
green <ewitulsk@calpoly.edu>
2021-11-29 10:41:38 -0800

    removed bodyParser

--- backend/backend_with_db.js
+++ backend/backend_with_db.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const {
   createNewUser,
   deleteUser,
@@ -27,9 +26,6 @@ const app = express();
 const port = 5000;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: false, limit: "50mb" }));
-app.use(bodyParser.json({ limit: "50mb", extended: true }));
-
 
 app.get("/", async (req, res) => {
   res.status(201).send("hello PolyGold user");
